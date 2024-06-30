c486c8d6829430dbc254f41d853f141d33325042
chLi <565353780@qq.com>
2024-07-01 00:07:56 +0800

    upgrade all related package versions

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,7 +1,13 @@
 cd ..
 git clone https://github.com/CVHub520/X-AnyLabeling.git XAL
 
-pip install -U pyqt5
+pip install -U opencv-contrib-python-headless pyqt5 imgviz natsort \
+	termcolor onnx onnxruntime qimage2ndarray lapx numpy pyyaml tqdm \
+	scipy shapely pyclipper filterpy tokenizers
+
+if [ "$(uname)" == "Linux" ]; then
+	pip install -U onnxruntime-gpu
+fi
 
 cd XAL
 pip install -e .
