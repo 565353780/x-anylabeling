0cd71675201759cef8813552d910d5673bc5cd75
chLi <565353780@qq.com>
2024-07-11 17:18:03 +0800

    fix linux system check typo error

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@ cd ..
 git clone https://github.com/CVHub520/X-AnyLabeling.git XAL
 
 pip install -U opencv-contrib-python-headless pyqt5 imgviz natsort \
-	termcolor onnx onnxruntime qimage2ndarray lapx numpy pyyaml tqdm \
-	scipy shapely pyclipper filterpy tokenizers
+  termcolor onnx onnxruntime qimage2ndarray lapx numpy pyyaml tqdm \
+  scipy shapely pyclipper filterpy tokenizers
 
-if [ "$(uname)" == "Linux" ]; then
-	pip install -U onnxruntime-gpu
+if [ "$(uname)" = "Linux" ]; then
+  pip install -U onnxruntime-gpu
 fi
 
 cd XAL
